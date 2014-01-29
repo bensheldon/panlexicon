7b537efbec03b57c2afbbfacc95d69bef99910f2
Ben Sheldon <bensheldon@gmail.com>
2014-01-29 13:20:03 -0800

    rails new panlexicon -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Panlexicon::Application.config.secret_key_base = '2f75ce54f37fd49bf829449e896134a6afe0432971f31221f1848e57c53d6747f32c6085e08325c54f6eb13c31d1ebb7de50aa5a5e7d7d95b621533fca2b1fee'
