76e67ec61d8514a102a7849184bb23a4f43e02bf
Ben Sheldon <bensheldon@gmail.com>
2014-11-02 12:07:23 -0800

    Enable Lograge in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ Panlexicon::Application.configure do
   # config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :info
+  # config.log_level = :info
+  config.lograge.enabled = true
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
