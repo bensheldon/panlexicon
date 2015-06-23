250eccd4ce9f4f170b274ed0316b9e64f6ecf9d3
Ben Sheldon <bensheldon@gmail.com>
2015-06-22 22:36:58 -0700

    Lint ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,14 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_filter :miniprofiler
+  before_action :miniprofiler
 
   private
 
   def miniprofiler
     # use /?miniprofiler_key=MINIPROFILER_KEY&pp=flamegraph in production
-    if params.fetch(:miniprofiler_key, nil) == Rails.application.secrets.miniprofiler_key
-      Rack::MiniProfiler.authorize_request
-    end
+    return if params.fetch(:miniprofiler_key, nil) != Rails.application.secrets.miniprofiler_key
+    Rack::MiniProfiler.authorize_request
   end
 end
