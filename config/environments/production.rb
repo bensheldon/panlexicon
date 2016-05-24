4aadbf8c9533ca968dd7deb1d1a8388e74c5f66f
Ben Sheldon <bensheldon@gmail.com>
2016-05-24 11:16:25 -0700

    Update log level to :info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Panlexicon::Application.configure do
   config.force_ssl = true
 
   # Set to :debug to see everything in the log.
-  config.log_level = :warn
+  config.log_level = :info
   config.lograge.enabled = true
 
   # Prepend all log lines with the following tags.
