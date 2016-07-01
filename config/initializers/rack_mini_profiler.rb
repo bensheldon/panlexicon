15b4227feca3232cb8e3fd156360b0aac1d8cd9a
Ben Sheldon <bensheldon@gmail.com>
2016-07-01 07:32:03 -0700

    Use redis backend for rack-mini-profiler in production

--- config/initializers/rack_mini_profiler.rb
+++ config/initializers/rack_mini_profiler.rb
@@ -9,3 +9,14 @@ if defined? HerokuDeflater
   Rails.application.middleware.swap(Rack::Deflater, Rack::MiniProfiler)
   Rails.application.middleware.swap(Rack::MiniProfiler, Rack::Deflater)
 end
+
+Rack::MiniProfiler.config.storage = Rack::MiniProfiler::MemoryStore
+if Rails.env.production? && ENV['REDIS_URL']
+  uri = URI.parse ENV['REDIS_URL']
+  Rack::MiniProfiler.config.storage_options = {
+    host: uri.host,
+    port: uri.port,
+    password: uri.password
+  }
+  Rack::MiniProfiler.config.storage = Rack::MiniProfiler::RedisStore
+end
