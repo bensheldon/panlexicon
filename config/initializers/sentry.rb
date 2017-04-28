d28e3ceea375598537cf2a013c092ccf05dc554a
Ben Sheldon <bensheldon@gmail.com>
2017-04-28 08:48:27 -0700

    Fix Sentry sanitized fields

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -4,6 +4,6 @@ if defined? Raven
     config.excluded_exceptions = Raven::Configuration::IGNORE_DEFAULT + %w[
       Rack::Timeout::RequestTimeoutError
     ]
+    config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   end
-  config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
 end
