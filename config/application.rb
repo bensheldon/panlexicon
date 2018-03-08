ba364a88c1b96e2dc27678219d55d006cf29cca2
Ben Sheldon <bensheldon@gmail.com>
2018-03-07 21:56:27 -0800

    Update Rails v5.1.1

--- config/application.rb
+++ config/application.rb
@@ -1,44 +1,18 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
-# Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Panlexicon
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.1
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Custom directories with classes and modules you want to be loaded
-    # http://blog.arkency.com/2014/11/dont-forget-about-eager-load-when-extending-autoload/
-    paths.add 'app/decorators/collections', eager_load: true
-    paths.add 'lib', glob: '*', eager_load: true
-    config.watchable_dirs['app/models/sql'] = [:sql]
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # 404 catch all route
-    # https://github.com/rails/rails/issues/671#issuecomment-1780159
-    config.after_initialize do |app|
-      app.routes.append { match '*path', via: [:get, :post], to: 'application#render_404' } unless config.consider_all_requests_local
-    end
-
-    config.action_mailer.default_url_options = {
-      host: Rails.application.secrets.hostname
-    }
   end
 end
