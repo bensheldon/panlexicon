093601fc4432a43b0885436384c08a4d53cb6409
Ben Sheldon <bensheldon@gmail.com>
2018-03-15 21:47:48 -0700

    Download Heroku executable directly for CircleCI

--- .circleci/heroku_setup.sh
+++ .circleci/heroku_setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-wget -qO- https://cli-assets.heroku.com/install-ubuntu.sh | sh
+wget https://cli-assets.heroku.com/branches/stable/heroku-linux-amd64.tar.gz
+sudo mkdir -p /usr/local/lib /usr/local/bin
+sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
+sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
 
 cat > ~/.netrc << EOF
 machine api.heroku.com
