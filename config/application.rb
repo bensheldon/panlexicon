5690c6164ff02d02433cae210a430c1ec7606321
Ben Sheldon [he/him] <bensheldon@gmail.com>
2019-08-16 07:29:07 -0700

    Upgrade to Rails 6.0.0rc2 (#239)

--- config/application.rb
+++ config/application.rb
@@ -9,11 +9,12 @@ Bundler.require(*Rails.groups)
 module Panlexicon
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
 
     # Custom directories with classes and modules you want to be loaded
     # http://blog.arkency.com/2014/11/dont-forget-about-eager-load-when-extending-autoload/
