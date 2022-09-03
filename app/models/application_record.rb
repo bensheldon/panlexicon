759b7ebc525d335a6398a8bf27c6d6033383bc10
Ben Sheldon [he/him] <bensheldon@gmail.com>
2022-09-03 07:46:42 -0700

    Fix linting error

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,7 @@
 
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-  def self.set_statement_timeout(seconds)
+  def self.set_statement_timeout(seconds) # rubocop:disable Naming/AccessorMethodName
     original_timeout = ActiveRecord::Base.connection.execute('SHOW statement_timeout').first['statement_timeout']
     ActiveRecord::Base.connection.execute("SET statement_timeout = '#{seconds.to_i}s'")
     yield
